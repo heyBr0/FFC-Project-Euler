cecd4aca4198e15b778f3c12f1e0b544115f3746
heyBr0 <benert@live.de>
2022-12-13 10:16:57 +0100

    part2

--- 4 Largest palindrome product.js
+++ 4 Largest palindrome product.js
@@ -7,11 +7,26 @@ largestPalindromeProduct(2) should return a number.
 largestPalindromeProduct(2) should return 9009.
 largestPalindromeProduct(3) should return 906609. */
 
-
 function largestPalindromeProduct(n) {
+  let maxPalindrome = 0;
+  let factor = 0;
 
-    return true;
+  for (let i = Math.pow(10, n - 1); i <= Math.pow(10, n); i++) {
+    let num = "" + i;
+    let newNum = num;
+    for (let j = num.length; j > 0; j--) {
+      newNum += num.charAt(j - 1);
+    }
+
+    for (let k = Math.pow(10, n - 1); k < Math.pow(10, n); k++) {
+      factor = Number(newNum) / k;
+      if (factor % 1 === 0 && String(factor).length == n) {
+        maxPalindrome = newNum;
+      }
+    }
   }
-  
- 
- console.log(largestPalindromeProduct(3))
+
+  return maxPalindrome;
+}
+
+console.log(largestPalindromeProduct(2))
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # FFC-Project-Euler
 
-https://www.freecodecamp.org/learn/coding-interview-prep/#project-euler
